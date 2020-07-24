3bf683b22c5d9d2c216bd1122ae9b91b552b3ec2
msd.khoshkam <msd.khoshkam@gmail.com>
2020-07-24 18:09:53 +0430

    Added home and detail nav graph

--- features/home/src/main/java/com/mkdev/home/HomeFragment.kt
+++ features/home/src/main/java/com/mkdev/home/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.home_fragment.*
 
 class HomeFragment : Fragment() {
 
@@ -19,5 +21,17 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeOpenDetailButton.setOnClickListener {
+            findNavController().navigate(
+                HomeFragmentDirections.actionHomeFragmentToNavGraphDetailFeature(
+                    "url"
+                )
+            )
+        }
     }
 }
